7e4eec0f06d7b111eb4c005c1383d9cfe4592e0b
Shradaya <shradayaramtel@lftechnology.com>
2024-05-28 09:03:47 +0545

    Use connection pools

--- app/dashapp/callbacks/leave_callbacks.py
+++ app/dashapp/callbacks/leave_callbacks.py
@@ -1,15 +1,14 @@
 import plotly.graph_objs as go
-from app.database import get_conn
 import dash_bootstrap_components as dbc
 from dash import dcc, html, Input, Output
 from psycopg2.extras import RealDictCursor
+from app.database import get_conn, put_conn
 
 from ..utils.sql_query import read_sql_query
 from ..template.leave_page.get_leave_balance import LEAVE_BALANCE_QUERY
 from ..template.leave_page.get_highest_leave_count import HIGHEST_LEAVE_COUNT_QUERY
 from ..template.leave_page.get_leave_count_per_weekday import LEAVE_COUNT_PER_WEEKDAY_QUERY
 from ..template.leave_page.get_late_applied_approved_leave_count import LATE_APPLIED_APPROVED_QUERY
-
 
 def execute_sql_query(query, **kwargs):
     selected_project = kwargs.get('selected_project')
@@ -19,17 +18,25 @@ def execute_sql_query(query, **kwargs):
     end_date = kwargs.get('end_date')
     fiscal_year = kwargs.get('fiscal_year')
     query, bind_params = read_sql_query(query,
-                                        selected_project = selected_project,
-                                        leave_type = leave_type,
-                                        department = department,
-                                        start_date = start_date,
-                                        end_date = end_date,
-                                        fiscal_year = fiscal_year)
-    with get_conn() as conn:
+                                        selected_project=selected_project,
+                                        leave_type=leave_type,
+                                        department=department,
+                                        start_date=start_date,
+                                        end_date=end_date,
+                                        fiscal_year=fiscal_year)
+    conn = None
+    try:
+        conn = get_conn()
         cursor = conn.cursor(cursor_factory=RealDictCursor)
         cursor.execute(query, bind_params)
         leaves_per_week_day = cursor.fetchall()
-    return leaves_per_week_day
+        return leaves_per_week_day
+    except Exception as e:
+        print(f"Database error occurred: {e}")
+        raise
+    finally:
+        if conn:
+            put_conn(conn)  # Release the connection back to the pool
 
 
 def register_leave_callbacks(app):
